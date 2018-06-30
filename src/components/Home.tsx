6442b0b99807765e04c613345dd36d453249cbb5
tom.finney25@googlemail.com <tom.finney25@googlemail.com>
2018-06-30 12:37:16 +0100

    better

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,7 +4,7 @@ export default class Home extends React.Component<any> {
   render() {
     return (
       <div>
-        <h1 style={{ color: 'white', textAlign: 'center' }}>
+        <h1 style={{ color: 'white', textAlign: 'center', marginTop: 0 }}>
           Movie Searcher
         </h1>
       </div>
