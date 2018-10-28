f697688e969c2fcf0afb3a26830d9fdc5a2b5b83
tom.finney@cd2solutions.co.uk <tom.finney@cd2solutions.co.uk>
2018-10-28 11:19:11 +0000

    start using react hook

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import query from "../stores/query";
 import { Loader } from "./Loader";
 import { Teasers } from "./Teasers";
 
-const Home = () => (
-  <div>
-    <h1 style={{ color: "white", textAlign: "center", marginTop: 0 }}>
-      Movie Searcher
-    </h1>
-    <div style={{ textAlign: "center" }}>
-      <form onSubmit={query.startSearch}>
-        <input type="text" onChange={query.setSearch} />
-        <button>Search</button>
-      </form>
+const Home = () => {
+  const [queryValue, setQueryValue] = useState("");
+
+  function handleSetQueryValue(e) {
+    setQueryValue(e.target.value);
+  }
+
+  return (
+    <div>
+      <h1 style={{ color: "white", textAlign: "center", marginTop: 0 }}>
+        Movie Searcher
+      </h1>
+      <div style={{ textAlign: "center" }}>
+        <form onSubmit={query.startSearch}>
+          <input
+            type="text"
+            value={queryValue}
+            onChange={handleSetQueryValue}
+          />
+          <button>Search</button>
+        </form>
+      </div>
+      {query.loading ? <Loader /> : undefined}
+      <Teasers />
     </div>
-    {query.loading ? <Loader /> : undefined}
-    <Teasers />
-  </div>
-);
+  );
+};
 
 export { Home };
